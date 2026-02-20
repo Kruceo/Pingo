2749d67175faf5c8b76736d7b7840bf17bdc491b
Kruceo <kruceo.com@gmail.com>
2026-02-20 11:00:53 -0300

    fix: add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const version = "0.0.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show binary version",
+	Long:  "Show the version of the pingo binary",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+}
